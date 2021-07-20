07ea77495ded52f0047b2067731b41a7d9eab0c7
xinyuan.yao <yao.ntno@gmail.com>
2021-07-20 22:19:35 +0900

    add tag selector

---
+++ app/packages/web/components/TagSelector.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import TagSelector from "./TagSelector";
+import Tag from "@sivic/core/tag";
+
+export default {
+  title: "TagSelector",
+  component: TagSelector,
+};
+const tags = [
+  Tag({
+    id: "t0",
+    name: "aaa",
+  }),
+  Tag({
+    id: "t1",
+    name: "bbb",
+  })
+]
+
+export const Primary = (args) =>( <TagSelector 
+  {...args} 
+  tags={tags}
+/> )
--- app/packages/web/store/ImageProcess.ts
+++ app/packages/web/store/ImageProcess.ts
@@ -14,9 +14,11 @@ import { parseISO } from "date-fns";
 import { Level } from "@sivic/web/store"
 import { Box } from "@sivic/core/box";
 import Editor from "@sivic/web/store/BoxEditor"
+import Tag from "@sivic/core/tag"
 import ImageStore from "@sivic/web/store/ImageStore"
 import File from "@sivic/core/file"
 import FileStore from "@sivic/web/store/FileStore"
+import TagStore from "@sivic/web/store/TagStore"
 
 export type ImageProcess = {
   image?: Image;
@@ -30,14 +32,13 @@ export type ImageProcess = {
 export const ImageProcess = (props: {
   api: RootApi;
   loading: <T>(fn: () => Promise<T>) => Promise<T>;
-  imageStore: ImageStore,
+  imageStore?: ImageStore,
   toast: ToastStore;
   onInit?: (imageId:string) => void
   editor: Editor
   fileStore?:FileStore,
 }): ImageProcess => {
   const { api, loading, toast, onInit, editor, imageStore } = props;
-
   const init = async (imageId:string) => {
     await loading(async () => {
       const image = await api.image.find({id:imageId})
@@ -54,7 +55,6 @@ export const ImageProcess = (props: {
       onInit && onInit(imageId)
     })
   }
-
   const detectBoxes = async () => {
     const { file } = self
     if(file === undefined) { return }
@@ -83,8 +83,8 @@ export const ImageProcess = (props: {
         toast.error(cropedImages)
         return
       }
-      imageStore.delete({parentId: self.image?.id || ""})
-      await imageStore.fetch({parentId: self.image?.id})
+      props.imageStore?.delete({parentId: self.image?.id || ""})
+      await props.imageStore?.fetch({parentId: self.image?.id})
       toast.info("saved")
     })
   }
