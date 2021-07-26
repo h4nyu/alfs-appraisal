b6f3720f386dbdc4ace718c03e382adf1089e2f5
xinyuan.yao <yao.ntno@gmail.com>
2021-07-27 00:09:29 +0900

    fix point

--- app/packages/core/point/index.ts
+++ app/packages/core/point/index.ts
@@ -1,5 +1,6 @@
 import { v4 as uuid } from 'uuid';
 export { default as FilterFn } from "./filter"
+export { default as LoadFn } from "./load"
 
 export type Point = {
   id: string;
---
+++ app/packages/server/routes/point.ts
@@ -0,0 +1,31 @@
+import { Sql } from "postgres";
+import { Store, Lock } from "@sivic/core";
+import { FastifyPlugin } from "fastify";
+import Point, {
+  FilterFn,
+  LoadFn,
+} from "@sivic/core/point";
+
+export const Routes = (props: {
+  store: Store;
+  lock: Lock;
+}): FastifyPlugin<{ prefix: string }> => {
+  const { store, lock } = props;
+  const filter = FilterFn(props)
+  const load = LoadFn(props)
+  return function (app, opts, done) {
+    app.post<{ Body: Parameters<FilterFn>[0] }>("/filter", {}, async (req, reply) => {
+      const res = await filter(req.body);
+      reply.send(res);
+    });
+    app.post<{ Body: Parameters<LoadFn>[0] }>("/load", {}, async (req, reply) => {
+      const res = await load({
+        ...req.body,
+        points: req.body.points.map(Point),
+      });
+      reply.send(res);
+    });
+    done();
+  };
+};
+export default Routes
