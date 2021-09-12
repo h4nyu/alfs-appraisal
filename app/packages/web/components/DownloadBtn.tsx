d4f860e7f2c7aca91a91c8d2afa13b2ba760346d
908kre <ce51046.higuchi@gmail.com>
2021-09-12 13:16:22 +0000

    fix

---
+++ app/packages/web/components/DownloadBtn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DownloadBtn = (props) => {
+  return (
+    <button className="button is-link is-light" { ...props }>
+      Download
+    </button>
+  );
+};
+export default Download;
+
