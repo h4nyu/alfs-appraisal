848e5204a7bafb74095167284b43b7bae592b145
yao <yao.ntno@gmail.com>
2021-10-23 10:28:25 +0000

    add modal

--- app/packages/web/components/Modal.tsx/index.stories.tsx
+++ app/packages/web/components/Modal.tsx/index.stories.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Component from "./";
-import Point from "@sivic/core/point"
-import { Workspace } from "@sivic/core/workspace";
+import Mock from "@sivic/web/components/Mock";
 
 export default {
   title: "Modal",
   component: Component,
 };
 
-export const Default = (args) => <Component {...args} />;
+export const Default = (args) => <Component {...args} >
+  <Mock />
+</Component>;
--- app/packages/web/components/Modal.tsx/index.tsx
+++ app/packages/web/components/Modal.tsx/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-export const Modal = (props) => {
+export const Modal = (props: {
+  isActive: boolean,
+  children?: React.ReactNode,
+  onClose?: VoidFunction,
+}) => {
   return (
-    <div className="modal">
+    <div className={`modal ${props.isActive ? "is-active" : "" }`}>
       <div className="modal-background"></div>
       <div className="modal-content">
+        {props.children}
       </div>
-      <button className="modal-close is-large" aria-label="close"></button>
+      <button 
+        className="modal-close is-large" 
+        aria-label="close"
+        onClick={props.onClose}
+      />
     </div>
   )
 }
